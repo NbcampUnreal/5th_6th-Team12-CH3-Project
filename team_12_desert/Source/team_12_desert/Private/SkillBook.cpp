b73b882def7119502989d45cfa8a4d55da2b2bab
ParkChanYeong <sidsru21131210@gmail.com>
2025-10-29 21:32:30 +0900

    스킬북 수정

--- team_12_desert/Source/team_12_desert/Private/SkillBook.cpp
+++ team_12_desert/Source/team_12_desert/Private/SkillBook.cpp
@@ -22,9 +22,7 @@ ASkillBook::ASkillBook()
 
 	Collision->OnComponentBeginOverlap.AddDynamic(this, &ASkillBook::OnItemOverlap);
 	Collision->OnComponentEndOverlap.AddDynamic(this, &ASkillBook::OnItemEndOverlap);
-	SetActorHiddenInGame(true);
-	SetActorEnableCollision(false);
-	SetActorTickEnabled(false);
+
 }
 
 void ASkillBook::OnItemOverlap(UPrimitiveComponent* OverlapPendComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
@@ -78,7 +76,6 @@ void ASkillBook::ActionSkill(TArray<AActor*> Actors, float time)
 			}
 		}
 	}
-
 }
 
 void ASkillBook::AddSkill(TObjectPtr<ASkillBase> NewSkill)
